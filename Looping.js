847ded36f48308763c644da00e574ee0e9579458
. <.>
2024-01-31 14:46:38 +0700

    first commit

---
+++ Looping.js
@@ -0,0 +1,11 @@
+//perulangan angka 1 sampai 50 //
+for(let i=0; i<=50; i++) {
+    console.log(i)
+}
+
+//perulangan angka 50 sampai 1//
+let l = 50
+while(l >=1 ) {
+    console.log(l)
+    l--
+}
